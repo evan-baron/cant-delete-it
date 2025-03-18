ca1a5f4755806b72ed15790d570cc0793b27f9c1
evan-baron <egbaron@gmail.com>
2025-03-18 17:20:29 -0600

    additional styling to leftside and opened viteconfig to allow local network testing on my phone

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       key: fs.readFileSync(path.resolve(__dirname, '../certs/localhost+2-key.pem')),
       cert: fs.readFileSync(path.resolve(__dirname, '../certs/localhost+2.pem'))
     },
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 5173
   }
 });
